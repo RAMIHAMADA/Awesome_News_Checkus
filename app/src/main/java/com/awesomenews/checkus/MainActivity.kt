e1827b4e6990d8d3b8c43200b37c11e25ecc4b50
Khamadekh Rami <khamadekh@gmail.com>
2023-01-20 16:05:02 +0700

    new App

---
+++ app/src/main/java/com/awesomenews/checkus/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.awesomenews.checkus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        val db = Firebase.firestore
+//        Log.e("COP", "ждем ответа с бд1 $db" )
+//        db.collection("types")
+//            .get()
+//            .addOnSuccessListener { result ->
+//                for (document in result) {
+//                    Log.e("COP", "ждем ответа бд2 $result" )
+//                }
+//            }
+//            .addOnFailureListener { exception ->
+//
+//            }
+
+
+
+    }
+
+}
+
